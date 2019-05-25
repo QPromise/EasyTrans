3051786093b0a22369d6357bf8a38b5cb199bd5f
QPromise <815221919@qq.com>
2019-05-25 16:54:59 +0800

    配置上传服务器

--- EasyTrans/settings.py
+++ EasyTrans/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8rk4uc)1xo38j1isjh!(a=2w#qs3r7y!fe7exqhd=mk&^h$26p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
